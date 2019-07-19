ef1ffd2e19de16beae1a3be3f749af94a340e803
anderskate <andrey.starozhilov@mail.ru>
2019-07-19 14:04:55 +0500

    repository publication

---
+++ scripts.py
@@ -0,0 +1,46 @@
+import random
+
+
+def fix_marks(schoolkid):
+    bed_marks = Mark.objects.filter(schoolkid=schoolkid, points__lt=4)
+    for mark in bed_marks:
+        mark.points = 5
+        mark.save()
+    return bed_marks
+
+
+def remove_chastisements(schoolkid):
+    chastisements = Сhastisement.objects.filter(schoolkid=schoolkid)
+    chastisements.delete()
+    return chastisements
+
+
+def adding_commendation(schoolkid, subject_title):
+    commendations = [
+        'Молодец!',
+        'Отлично!',
+        'Ты меня приятно удивил!',
+        'Талантливо!',
+        'Так держать!',
+        'Здорово!',
+        'Я вижу, как ты стараешься!',
+        'Я поражен!',
+        'Это как раз то, что нужно!',
+        'Великолепно!',
+        'Я тобой горжусь!'
+    ]
+    lessons = Lesson.objects.filter(
+        year_of_study=schoolkid.year_of_study,
+        group_letter=schoolkid.group_letter,
+        subject__title=subject_title
+    )
+    random_commendation = random.choice(commendations)
+    random_lesson = random.choice(lessons)
+    commendation = Commendation.objects.create(
+        text=random_commendation,
+        created=random_lesson.date,
+        schoolkid=schoolkid,
+        subject=random_lesson.subject,
+        teacher=random_lesson.teacher
+    )
+    return commendation
